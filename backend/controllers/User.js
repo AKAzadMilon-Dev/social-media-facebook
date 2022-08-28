36176c16f6f1b42750af5b4e3f224f1adf10ffda
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-08-28 21:38:51 +0600

    user text length max or min validation and bcrypt hash password

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,5 +1,9 @@
-const { validationEmail } = require("../helpers/Validation.js");
+const {
+  validationEmail,
+  validationLength,
+} = require("../helpers/Validation.js");
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
   try {
@@ -27,6 +31,29 @@ exports.register = async (req, res) => {
         message: "This email already in use. Please try another email.",
       });
     }
+
+    // Text length check
+    if (!validationLength(first_name,3,30)) {
+      return res.status(400).json({
+        message: "Your First name must be min 3 letter and max 30 letter",
+      });
+    }
+
+    if (!validationLength(last_name,3,30)) {
+      return res.status(400).json({
+        message: "Your last name must be min 3 letter and max 30 letter",
+      });
+    }
+
+    if (!validationLength(password, 6, 40)) {
+      return res
+        .status(400)
+        .json({ message: "Your password must be 6 characters" });
+    }
+
+    const cryptPassword = await bcrypt.hash(password, 12);
+    console.log(cryptPassword);
+
     return;
 
     const user = await new User({
--- backend/helpers/Validation.js
+++ backend/helpers/Validation.js
@@ -1,5 +1,14 @@
+const { text } = require("express");
+
 exports.validationEmail = (email) => {
   return String(email)
     .toLowerCase()
     .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 };
+
+exports.validationLength = (text, max, min) => {
+    if (text.length > max || text.length < min){
+        return false;
+    };
+    return true;
+}
