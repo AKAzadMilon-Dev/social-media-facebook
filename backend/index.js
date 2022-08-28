f8dfb3f4e169b111568707694442f6af0f86bb56
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-08-28 14:46:33 +0600

    dynamic-route-link setup

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
-const user = require('./routes/user.js')
+const {readdirSync} = require('fs');
 
 dotenv.config();
 
 app.use(cors());
 
-app.use('/', user);
+readdirSync('./routes').map((fileName) => app.use('/', require('./routes/'+fileName)))
 
 const port = process.env.PORT || 8000;
 
