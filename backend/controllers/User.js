1f636a7c18461240816b191628d3a8ecf3cd89fe
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-08-28 19:23:41 +0600

    server error message

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,30 +1,34 @@
 const User = require("../models/User.js")
 
 exports.register = async (req, res)=>{
-    const {
-        first_name,
-        last_name,
-        username,
-        email,
-        password,
-        birthYear,
-        birthMonth,
-        birthDay,
-        gender,
-    } = req.body
-
-    const user = await new User({
-        first_name,
-        last_name,
-        username,
-        email,
-        password,
-        birthYear,
-        birthMonth,
-        birthDay,
-        gender,
-    }).save()
-
-    res.json(user)
+    try{
+        const {
+            first_name,
+            last_name,
+            username,
+            email,
+            password,
+            birthYear,
+            birthMonth,
+            birthDay,
+            gender,
+        } = req.body
+    
+        const user = await new User({
+            first_name,
+            last_name,
+            username,
+            email,
+            password,
+            birthYear,
+            birthMonth,
+            birthDay,
+            gender,
+        }).save()
+    
+        res.json(user)
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
 
 };
