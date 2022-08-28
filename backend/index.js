838e91daf6d9f3d0011317728f92cc13a4c4c694
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-08-28 15:12:59 +0600

    database connected with inistall

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = express();
 const {readdirSync} = require('fs');
 
 dotenv.config();
 
+// Middileware
 app.use(cors());
 
-readdirSync('./routes').map((fileName) => app.use('/', require('./routes/'+fileName)))
+// Routes
+readdirSync('./routes').map((fileName) => app.use('/', require('./routes/'+fileName)));
+
+// Database
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("database connected");
+});
 
 const port = process.env.PORT || 8000;
 
