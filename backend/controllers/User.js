2c6dbd959e0b7bc60ecf83f1a550ec3b26894317
AK Azad Milon <akazadmilon.dev@gmail.com>
2022-09-14 13:45:50 +0600

    node.js error handleing

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -94,15 +94,19 @@ exports.register = async (req, res) => {
 };
 
 exports.activateAccount = async (req, res) => {
-  const { token } = req.body;
-  const user = jwt.verify(token, process.env.TOKEN_SECRET);
-  const check = await User.findById(user.id);
-  if (check.verified == true) {
-    return res
-      .status(400)
-      .json({ message: "This account is already activated" });
-  } else {
-    await User.findByIdAndUpdate(user.id, { verified: true });
-    return res.status(200).json({ message: "Account has been activated" });
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+    if (check.verified == true) {
+      return res
+        .status(400)
+        .json({ message: "This account is already activated" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res.status(200).json({ message: "Account has been activated" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
